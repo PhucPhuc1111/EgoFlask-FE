33eaae5f2e2919edf314fd739be5c1cb6db6dc79
Ngo Quang Thang <ngoquangthang2003@gmail.com>
2024-08-12 00:37:43 +0700

    Init react remix project

--- EgoFlask-FE/vite.config.ts
+++ EgoFlask-FE/vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { vitePlugin as remix } from "@remix-run/dev";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    remix(), tsconfigPaths(),
+  ],
+});
