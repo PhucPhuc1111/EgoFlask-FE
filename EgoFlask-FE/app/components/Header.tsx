72d48554463bc461f08f1b73faad5159e434e54d
Ngo Quang Thang <ngoquangthang2003@gmail.com>
2024-08-12 17:56:42 +0700

    Fix ui

--- EgoFlask-FE/app/components/Header.tsx
+++ EgoFlask-FE/app/components/Header.tsx
@@ -25,36 +25,40 @@ export const Header = () => {
   ]
 
   return (
-    <header className="flex flex-col items-center justify-center gap-2">
-      <div
-        className="top-bar w-full h-[37px] flex flex-rows items-center justify-center py-2 text-white bg-[#0055C3]"
-      >
-        Miễn phí vận chuyển cho đơn hàng trên 1.000.000 VND
-      </div>
-      <div
-        className="nav-bar flex flex-rows items-center justify-between h-12 px-16 w-full"
-      >
-        <nav className="flex flex-row justify-start items-center gap-8">
-          {_.map(navBar, (item, index) => (
-            <NavLink key={index} to={item.link} className={({ isActive }) =>
-              `flex flex-rows text-base hover:no-underline ${isActive ? 'text-[#0055C3] font-bold' : 'text-black hover:text-[#0255C3]'}`
-            }>
-              {item.title}
-            </NavLink>
-          ))}
-        </nav>
-        <img
-          src="/images/Logo.png"
-          alt="Logo"
-          className="logo w-[157px] h-[45px] cursor-pointer lg:mr-56"
-          onClick={() => navigate('/')}
-        />
+    <header>
+      <div className="flex flex-col items-center justify-center gap-2">
         <div
-          className="nav-act flex flex-rows gap-4"
+          className="top-bar w-full h-[37px] flex flex-rows items-center justify-center py-2 text-white bg-[#0055C3]"
         >
-          <IoSearch className="cursor-pointer w-6 h-6" />
-          <IoPersonOutline className="cursor-pointer w-6 h-6" />
-          <IoCartOutline className="cursor-pointer w-6 h-6" />
+          Miễn phí vận chuyển cho đơn hàng trên 1.000.000 VND
+        </div>
+        <div
+          className="nav-bar flex flex-rows items-center justify-between w-full h-12 px-16"
+        >
+          <nav className="flex flex-row justify-start items-center space-x-8">
+            {_.map(navBar, (item, index) => (
+              <NavLink key={index} to={item.link} className={({ isActive }) =>
+                `flex flex-rows text-base hover:no-underline ${isActive ? 'text-[#0055C3] font-bold' : 'text-black hover:text-[#0255C3]'}`
+              }>
+                {item.title}
+              </NavLink>
+            ))}
+          </nav>
+          <div className="flex items-center -translate-x-1/2">
+            <img
+              src="/images/Logo.png"
+              alt="Logo"
+              className="logo w-[157px] h-[45px] cursor-pointer"
+              onClick={() => navigate('/')}
+            />
+          </div>
+          <div
+            className="nav-act flex flex-rows gap-4"
+          >
+            <IoSearch className="cursor-pointer w-6 h-6" />
+            <IoPersonOutline className="cursor-pointer w-6 h-6" />
+            <IoCartOutline className="cursor-pointer w-6 h-6" />
+          </div>
         </div>
       </div>
     </header>
