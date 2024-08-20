89153cd6b9e66c284203a61893303385dd377c76
Baolngse171848 <giabaole123456789@gmail.com>
2024-08-20 12:11:31 +0700

    add hook form

--- EgoFlask-FE/app/routes/verify.email.tsx
+++ EgoFlask-FE/app/routes/verify.email.tsx
@@ -1,41 +1,73 @@
-import { Link } from "@remix-run/react"
-import { SubFooter } from "~/components"
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Link } from "@remix-run/react";
+import { useForm } from "react-hook-form";
 
-const verifyEmail = () => {
+import { InferType, number, object } from "yup";
+import { SubFooter } from "~/components";
+
+let schema = object({
+  emailCode: number().required("Vui lòng nhập mã xác nhận"),
+});
+const resolver = yupResolver(schema);
+type EmailCodeForm = InferType<typeof schema>;
+
+export default function verifyEmail() {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<EmailCodeForm>({
+    resolver,
+    mode: "onChange",
+  });
+
+  const onSubmit = (data: EmailCodeForm) => {
+    console.log("data", data);
+  };
+
   return (
     <main className="mt-[--m-header-top]">
-    <div className="w-full max-w-md p-8 bg-white shadow-md rounded-lg mx-auto mt-36">
-    <h2 className="text-2xl font-semibold text-gray-800 mb-6">Nhập mã xác minh</h2>
-    
-    <form className="space-y-6">
-     
-      <div>
-        <label htmlFor="verification-code" className="block text-sm font-medium text-center text-gray-700 mb-2">
-          Mã xác minh đã được gửi đến Email
-          <p className=" font-semibold">nguyenvanA@gmail.com</p>
-        </label>
-        <input
-          type="text"
-          id="verification-code"
-          name="verification-code"
-          placeholder="Nhập mã xác minh"
-          className="block w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-          required
-        />
-      </div>
-      <p className="text-center">Bạn vẫn chưa nhận được? <a className="text-[#0055C3]" href="/profile">Gửi lại</a></p>
-      <Link to={'/profile'}><button
-        type="submit"
-        className="w-full px-4 py-2 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
-      >
-        Xác minh
-      </button></Link>
-      
-    </form>
-  </div>
-  <div><SubFooter/></div>
-  </main>
-  )
-}
+      <div className="w-full max-w-md p-8 bg-white shadow-md rounded-lg mx-auto mt-36">
+        <h2 className="text-2xl font-semibold text-gray-800 mb-6">
+          Nhập mã xác minh
+        </h2>
 
-export default verifyEmail
+        <form
+          className="space-y-6"
+          onSubmit={handleSubmit(onSubmit)}
+          method="post"
+        >
+          <span className="block text-sm font-medium text-center text-gray-700 mb-2">
+            Mã xác minh đã được gửi đến Email
+            <p className=" font-semibold">nguyenvanA@gmail.com</p>
+          </span>
+          <input
+            type="number"
+            {...register("emailCode")}
+            placeholder="Nhập mã xác minh"
+            className="block w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+            required
+          />
+
+          <p className="text-center">
+            Bạn vẫn chưa nhận được?{" "}
+            <a className="text-[#0055C3]" href="/profile">
+              Gửi lại
+            </a>
+          </p>
+          {/* <Link to={'/profile'}> */}
+          <button
+            type="submit"
+            className="w-full px-4 py-2 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          >
+            Xác minh
+          </button>
+          {/* </Link> */}
+        </form>
+      </div>
+      <div>
+        <SubFooter />
+      </div>
+    </main>
+  );
+}
--- EgoFlask-FE/app/routes/verify.phone.tsx
+++ EgoFlask-FE/app/routes/verify.phone.tsx
@@ -1,13 +1,38 @@
+import { yupResolver } from "@hookform/resolvers/yup";
 import { Link } from "@remix-run/react"
+import { useForm } from "react-hook-form";
+import { InferType, number, object } from "yup";
 import { SubFooter } from "~/components"
 
-const verifyPhone= () => {
+
+let schema = object({
+  OTPcode: number().required("Vui lòng nhập mã xác nhận"),
+});
+const resolver = yupResolver(schema);
+type OTPCodeForm = InferType<typeof schema>;
+
+
+export default function verifyPhone ()  {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<OTPCodeForm>({
+    resolver,
+    mode: "onChange",
+  });
+
+  const onSubmit = (data: OTPCodeForm) => {
+    console.log("data", data);
+  };
     return (
       <main className="mt-[--m-header-top] ">
       <div className="w-full max-w-md p-8 bg-white shadow-md rounded-lg mx-auto mt-36 space-y-12 ">
       <h2 className="text-2xl font-semibold text-gray-800 mb-6 mt0">Nhập mã OTP</h2>
       
-      <form className="space-y-6">
+      <form className="space-y-6"
+      onSubmit={handleSubmit(onSubmit)}
+      method="post">
        
         <div>
           <label htmlFor="verification-code" className=" text-center block text-sm font-medium text-gray-700 mb-2">
@@ -15,23 +40,23 @@ const verifyPhone= () => {
           <p className=" font-semibold text-lg">087238919</p>
           </label>
           <input
-            type="text"
-            id="verification-code"
-            name="verification-code"
-            placeholder="Nhập mã OTP"
-            className="block w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-            required
-          />
+          type="number"
+          {...register("OTPcode")}
+          placeholder="Nhập mã OTP"
+          className="block w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+          required
+        />
         </div>
         
         <p className="text-center">Bạn vẫn chưa nhận được? <a className="text-[#0055C3]" href="/profile">Gửi lại</a></p>
-        <Link to={'/profile'}>
+        {/* <Link to={'/profile'}> */}
         <button
           type="submit"
           className="w-full px-4 py-2 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
         >
           Xác minh
-        </button></Link>
+        </button>
+        {/* </Link> */}
         
       </form>
     </div>
@@ -40,4 +65,3 @@ const verifyPhone= () => {
     )
   }
   
-  export default verifyPhone
