cc3a9b3404d59416ca3bd52481fedc7ae0ec1b56
Ngo Quang Thang <ngoquangthang2003@gmail.com>
2024-10-14 17:09:52 +0700

    Add profile card and address card for admin order

--- app/routes/admin.order.tsx
+++ app/routes/admin.order.tsx
@@ -1,4 +1,4 @@
-import { Pagination, PaginationProps } from "antd";
+import { Image, Pagination, PaginationProps } from "antd";
 import _ from "lodash";
 import { useCallback, useMemo, useState } from "react";
 import { IoSearchOutline } from "react-icons/io5"
@@ -52,7 +52,7 @@ export default function AdminOrder() {
     return (
       <div className="flex flex-row items-center justify-between gap-2">
         <div className="bg-[#E8E8E4] self-center">
-          <img src="/images/products/bottle-1.png" alt="bottle" className="w-[89px] h-[122px]" />
+          <Image src="/images/products/bottle-1.png" alt="bottle" width={89} height={122} className="w-[89px] h-[122px]" />
         </div>
         <div className="flex flex-1 flex-col justify-between text-sm">
           <div className="flex flex-row justify-between">
@@ -71,11 +71,43 @@ export default function AdminOrder() {
               <span>10000 VND</span>
             </div>
           </div>
-          <div className="border-2 border-[#0055C3] rounded-full my-2"/>
+          <div className="border-2 border-[#0055C3] rounded-full my-2" />
           <div className="w-full flex flex-row items-center justify-between">
             <span className="text-[#0055C3] font-bold">Tổng tiền: </span>
             <span className="text-[#0055C3] font-bold">100000 VND </span>
           </div>
+        </div>
+      </div>
+    )
+  }
+
+  const AddressCard = () => {
+    return (
+      <div className="flex flex-col items-start justify-start text-start gap-4">
+        <h4 className="text-black">
+          Nguyễn Văn A
+        </h4>
+        <span className="text-[#7D7474]">
+          (+84) 905 245 344
+        </span>
+        <span className="text-[#7D7474]">
+          1472 Đ. Võ Văn Kiệt, Phường 3, Quận 6, Hồ Chí Minh
+        </span>
+      </div>
+    )
+  }
+
+  const ProfileCard = ({ email, name }: { name: string, email: string }) => {
+    return (
+      <div className="flex items-center justify-center gap-2">
+        <Image src="/images/avatar.png" alt="avatar" width={32} height={32} className="w-8 h-8 self-start"/>
+        <div className="flex flex-col items-start gap-1">
+          <span>
+            {name}
+          </span>
+          <span>
+            ({email})
+          </span>
         </div>
       </div>
     )
@@ -99,7 +131,7 @@ export default function AdminOrder() {
           <thead className="bg-[#0055C3] text-white">
             <tr className="w-full">
               <th className="border border-white">Mã đơn hàng</th>
-              <th className="border border-white">Tên</th>
+              <th className="border border-white w-1/3">Tên</th>
               <th className="border border-white w-1/3">Sản phẩm</th>
               <th className="border border-white">Địa chỉ nhận hàng</th>
               <th className="border border-white">Tình trạng</th>
@@ -109,11 +141,14 @@ export default function AdminOrder() {
             {_.map(filteredOrder, (item, index) => (
               <tr key={index} className="text-center">
                 <td className="border-2 border-[#0055C3] border-opacity-70">{item.orderId}</td>
-                <td className="border-2 border-[#0055C3] border-opacity-70">{"Nguyễn Văn A"}</td>
+                <td className="border-2 border-[#0055C3] border-opacity-70 w-1/3 overflow-auto">
+                  <ProfileCard name={"Ngô Quang Thắng"} email="thangnqse173445@fpt.edu.vn" />
+                </td>
                 <td className="border-2 border-[#0055C3] border-opacity-70 w-1/3 p-2">
                   <ProductCard />
                 </td>
                 <td className="border-2 border-[#0055C3] border-opacity-70 p-2">
+                  <AddressCard />
                 </td>
                 <td className="border-2 border-[#0055C3] border-opacity-70 py-2 px-4">
                   <select name="" className="border-1 border-[#0055C3] w-fit h-fit rounded-md">
