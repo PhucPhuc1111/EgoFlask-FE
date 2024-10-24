d8f601ff21743d5c9a5d6923b234665419977d54
Ngo Quang Thang <ngoquangthang2003@gmail.com>
2024-10-24 17:12:34 +0700

    Fix responsive on register page

--- app/routes/register.tsx
+++ app/routes/register.tsx
@@ -163,10 +163,10 @@ import { criteria } from "~/components/utils";
 import { registerAccount } from "~/data/user";
 
 let schema = object({
-  firstName: string().required(),
+  firstName: string().required("Vui lòng nhập tên của bạn"),
   lastName: string().required("Vui lòng nhập họ của bạn"),
   email: string().email("Email chưa hợp lệ").required("Vui lòng nhập email của bạn"),
-  phone: number().required(),
+  phone: number().required("Vui lòng nhập số điện thoại của bạn"),
   password: string()
     .min(6, 'Mật khẩu phải có ít nhất 6 ký tự')
     .max(25, "Mật khẩu không được quá 25 ký tự")
@@ -222,20 +222,21 @@ export default function Register() {
             placeholder="Họ"
             className="w-full focus:ring-0 border-0 border-b-2 border-[#E6E6E0] transition duration-200 ease-in"
           />
-          {errors.lastName && <span className="text-red-500 font-bold">{errors.lastName.message}</span>}
+          {errors.lastName && <span className="text-red-500 font-bold text-xs sm:text-sm">{errors.lastName.message}</span>}
 
           <input type="text"
             placeholder="Tên"
             className="w-full focus:ring-0 border-0 border-b-2 border-[#E6E6E0] transition duration-200 ease-in"
             {...register('firstName')}
           />
+          {errors.firstName && <span className="text-red-500 font-bold text-xs sm:text-sm">{errors.firstName.message}</span>}
 
           <input type="email"
             placeholder="Email"
             className="w-full focus:ring-0 border-0 border-b-2 border-[#E6E6E0] transition duration-200 ease-in"
             {...register('email')}
           />
-          {errors.email && <span className="text-red-500 font-bold">{errors.email.message}</span>}
+          {errors.email && <span className="text-red-500 font-bold text-xs sm:text-sm">{errors.email.message}</span>}
 
           <input type="tel"
             placeholder="Số điện thoại"
@@ -243,7 +244,7 @@ export default function Register() {
             className="w-full focus:ring-0 border-0 border-b-2 border-[#E6E6E0] transition duration-200 ease-in"
             {...register('phone')}
           />
-          {errors.phone && <span className="text-red-500 font-bold">{errors.phone.message}</span>}
+          {errors.phone && <span className="text-red-500 font-bold text-xs sm:text-sm">{errors.phone.message}</span>}
 
           <div className="relative w-full">
             <input
@@ -267,14 +268,14 @@ export default function Register() {
                   {criterion.test(password) ? (
                     <div className="flex items-center gap-2" key={index}>
                       <IoCheckmarkCircle className="w-5 h-5 text-green-500" />
-                      <p className={`text-green-500 font-bold`}>
+                      <p className={`text-green-500 font-bold text-xs sm:text-sm`}>
                         {criterion.label}
                       </p>
                     </div>
                   ) : (
                     <div className="flex items-center gap-2" key={index}>
-                      <IoCloseCircle className="w-5 h-5 text-red-500" />
-                      <p className={`text-red-500 font-bold`}>
+                      <IoCloseCircle className="w-5 h-5 text-gray-500" />
+                      <p className={`text-gray-500 font-bold text-xs sm:text-sm`}>
                         {criterion.label}
                       </p>
                     </div>
@@ -288,7 +289,6 @@ export default function Register() {
             <input
               type={showConfirmPassword ? 'text' : 'password'}
               placeholder="Xác nhận mật khẩu"
-              required
               className="w-full focus:ring-0 border-0 border-b-2 border-[#E6E6E0] transition duration-200 ease-in"
               {...register('confirmPassword')}
             />
@@ -298,11 +298,11 @@ export default function Register() {
               <IoEye onClick={() => setShowConfirmPassword(!showConfirmPassword)} className='transition-opacity duration-300 ease-in-out opacity-100 absolute right-3 top-0 mt-3 cursor-pointer size-6 text-[#465166] hover:text-black' />
             )}
           </div>
-          {errors.confirmPassword && <span className="text-red-500 font-bold">{errors.confirmPassword.message}</span>}
+          {errors.confirmPassword && <span className="text-red-500 font-bold text-xs sm:text-sm">{errors.confirmPassword.message}</span>}
 
-          <button type="submit" disabled={isSubmitting} className="flex items-center justify-center uppercase active:bg-blue-800 hover:bg-blue-900 disabled:bg-blue-900 bg-[#0055C3] w-full py-4 rounded-lg text-white font-semibold text-lg sm:text-xl">
-            {isSubmitting && <img src="/icons/loading.svg" alt="" className="w-10 h-10" />}
-            <span>
+          <button type="submit" disabled={isSubmitting} className="flex items-center justify-center uppercase active:bg-blue-800 hover:bg-blue-900 disabled:bg-blue-900 bg-[#0055C3] w-full py-2 sm:py-4 rounded-lg text-white font-semibold text-lg sm:text-xl">
+            {!isSubmitting && <img src="/icons/loading.svg" alt="" className="w-5 h-5 sm:w-10 sm:h-10" />}
+            <span className="text-sm sm:text-xl">
               Tạo tài khoản
             </span>
           </button>
