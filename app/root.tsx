b79f4bea2824141ccd5c7b1efd5528dfab28f0c9
Ngo Quang Thang <ngoquangthang2003@gmail.com>
2024-10-24 10:34:32 +0700

    Add custom error page

--- app/root.tsx
+++ app/root.tsx
@@ -1,10 +1,12 @@
 import {
   isRouteErrorResponse,
+  Link,
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useNavigate,
   useRouteError,
 } from "@remix-run/react";
 import "./tailwind.css";
@@ -27,6 +29,50 @@ export const links: LinksFunction = () => [
   { rel: "icon", href: "/images/SquareLogo.png" },
 ];
 
+export function ErrorBoundary() {
+  const navigate = useNavigate();
+  const error = useRouteError();
+  return (
+    <html>
+      <head>
+        <title>Oops!</title>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <main className="h-screen w-full flex flex-col justify-center items-center bg-[#1A2238]">
+          <h1 className="text-9xl font-extrabold text-white tracking-widest">
+            {isRouteErrorResponse(error) ? error.status :
+              error instanceof Error ?
+                error.message :
+                "Unknown Error"}</h1>
+          <div className="bg-[#FF6A3D] text-slate-900 px-2 text-sm rounded rotate-12 absolute">
+            {isRouteErrorResponse(error)
+              ? `${error.status} ${error.statusText}`
+              : error instanceof Error
+                ? error.message
+                : "Unknown Error"}
+          </div>
+          <button onClick={() => navigate(-1)} className="mt-5">
+            <a
+              className="relative no-underline hover:no-underline inline-block text-sm font-medium text-[#FF6A3D] group active:text-orange-500 focus:outline-none focus:ring"
+            >
+              <span
+                className="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#FF6A3D] group-hover:translate-y-0 group-hover:translate-x-0"
+              ></span>
+
+              <span className="relative block px-8 py-3 bg-[#1A2238] border border-current">
+                Go Back
+              </span>
+            </a>
+          </button>
+        </main>
+        <Scripts />
+      </body>
+    </html>
+  );
+}
+
 export default function App() {
   const [queryClient] = useState(
     () =>
@@ -47,7 +93,6 @@ export default function App() {
         }),
       }),
   );
-  const error = useRouteError();
 
   return (
     <html lang="vi">
@@ -64,13 +109,6 @@ export default function App() {
         <body className="w-full h-full overflow-y-auto overflow-x-hidden">
           <div id="wrapper">
             <Header />
-            <h1>
-              {isRouteErrorResponse(error)
-                ? `${error.status} ${error.statusText}`
-                : error instanceof Error
-                  ? error.message
-                  : "Unknown Error"}
-            </h1>
             <Outlet />
             <Footer />
           </div>
