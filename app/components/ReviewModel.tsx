2596fc686257d7c5091ab008b81e780f49624d8c
Ngo Quang Thang <ngoquangthang2003@gmail.com>
2024-10-25 16:38:15 +0700

    Fix review model in mobile version

--- app/components/ReviewModel.tsx
+++ app/components/ReviewModel.tsx
@@ -31,19 +31,25 @@ export const ReviewModal: React.FC<ReviewModalProps> = ({
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center" onClick={handleClickOutside}>
-      <div className="bg-white rounded-lg p-6 min-h-[550px] min-w-[500px] relative">
+      <div className="bg-white rounded-lg p-6 min-w-[500px] relative"
+        style={{
+          height: 'calc(100vh - 120px)',
+        }}
+      >
         <h2 className="flex justify-center text-lg font-bold mb-4">
           Xem trước thiết kế
         </h2>
 
-        <Model
-          topImage={top?.imageUrl}
-          bodyImage={body?.imageUrl}
-          strapImage={strap?.imageUrl}
-          engrave={engrave}
-          engravePosition={engravePosition}
-          width="500px"
-        />
+        <div className="absolute inset-0">
+          <Model
+            topImage={top?.imageUrl}
+            bodyImage={body?.imageUrl}
+            strapImage={strap?.imageUrl}
+            engrave={engrave}
+            engravePosition={engravePosition}
+            width="500px"
+          />
+        </div>
       </div>
     </div>
   );
